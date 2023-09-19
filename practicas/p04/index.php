136f9ab72cda25f47657cc1019fcd4fbaa456bbb
RM <regina.avelinof@alumno.buap.mx>
2023-09-19 00:55:19 -0600

    index php ejercicio 2 cargado

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> <!--Especificamos la versión de xhtml 1.1-->
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es"> <!--Especificamos namespace y el idioma-->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,20 +23,52 @@
         }
     ?>
 
-    <h2>Ejemplo de POST</h2>
-    <form action="http://localhost/tecweb/practicas/p04/index.php" method="post">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        <input type="submit">
-    </form>
-    <br>
+<h2>Ejercicio 2:Crea un programa para la generación repetitiva de 3 números aleatorios hasta obtener una
+    secuencia compuesta por: impar,par,impar.</h2>
     <?php
-        if(isset($_POST["name"]) && isset($_POST["email"]))
+    ///Definimos funciones a usar:
+    function generar_numeros() //Funcion que genera los numeros aleatorios
+    {
+        return rand(1, 1000); //Función random, tiene como parametros de entrada: el numero de inicio y el de final
+    }
+    function secuencia_cumplida($secuencia) 
+    {
+        return $secuencia[0] % 2 != 0 && $secuencia[1] % 2 == 0 && $secuencia[2] % 2 != 0;
+    }    // Verificamos si en cada posición del arreglo se cumple con la secuencia impar, par, impar
+ 
+   ///Codigo para generar la matriz:
+    $matriz = [];
+    $iteraciones = 0;//contador de iteraciones
+    $numeros_obtenidos = 0; //contador de numeros generados hasta obtener las 4 secuencias que cumplan con la condicion impar,par,impar
+    while (true) 
+    { 
+        $numeros_obtenidos++; 
+        $secuencia = [generar_numeros(), generar_numeros(), generar_numeros()]; /// insertamos en el arreglo "$secuencia" los numeros generados
+        if (secuencia_cumplida($secuencia)) ///evaluamos la secuencia en la funcion secuencia_cumplida
         {
-            echo $_POST["name"];
-            echo '<br>';
-            echo $_POST["email"];
+            $matriz[] = $secuencia; //si la secuencia se cumple se agrega a la matriz
+            $iteraciones++;
+            if ($iteraciones == 4) // salimos del ciclo si hay 4 iteraciones que cumplan con la secuencia
+                { 
+                    break;
+                }
         }
+    }
+    // Impresion de matriz 
+    echo "<table border='1'>";
+    foreach ($matriz as $fila) 
+    {
+        echo "<tr>";
+        foreach ($fila as $valor) 
+        {
+            echo "<td>$valor</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+    echo "$numeros_obtenidos números generados en $iteraciones iteraciones";
     ?>
+
+
 </body>
 </html>
